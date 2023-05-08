ca483a9c8e98f3f83bef27aac5f258a40edfbb87
Pedro Henrique <pedrowkriss@gmail.com>
2023-05-08 02:21:34 -0700

    Finalizado

---
+++ Models/HighSchoolDto.cs
@@ -0,0 +1,9 @@
+namespace HighSchool.Models
+{
+    public class HighSchoolDto
+    {
+        public List<Habilidade> Habilidades { get; set; }
+        public List<Personagem> Personagens { get; set; }
+
+    }
+}
